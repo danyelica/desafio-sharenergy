0c8eff95f0ae5ea976d7374988914afd3cc56f63
Danyélica <danydayane67@gmail.com>
2023-01-05 15:25:51 -0300

    feito o resto do crud e corrigido o login

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const routes = express();
-const { register, login } = require("./controllers/users");
+const {
+  register,
+  login,
+  list,
+  findById,
+  update,
+  remove,
+} = require("./controllers/users");
 
-routes.get("/", (req, res) => {
-  res.json({ mensagem: "api rodando" });
-});
-
+routes.get("/users", list);
+routes.get("/users/:id", findById);
 routes.post("/sing-up", register);
 routes.post("/login", login);
+routes.put("/users/:id", update);
+routes.delete("/users/:id", remove);
 
 module.exports = routes;
